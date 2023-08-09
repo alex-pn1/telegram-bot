652bbefc849fc64adb6d36e8bfb3ad83da7dd844
Sasha <alex_penza@list.ru>
2023-08-09 23:27:32 +0300

    done

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -5,8 +5,8 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.service.NotificationTaskService;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -16,8 +16,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
+    private final NotificationTaskService service;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationTaskService service) {
+        this.telegramBot = telegramBot;
+        this.service = service;
+    }
 
     @PostConstruct
     public void init() {
@@ -26,9 +31,23 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
+
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            Long chatId = update.message().chat().id();
             // Process your updates here
+            String message = update.message().text();
+
+            if (update.message().text().equals("/start")){
+                if (service.replyStart(chatId) == null) {
+                    service.sendSomethingWrong(chatId);
+                }
+            } else {
+                if (service.saveTask(chatId, message) == null) {
+                    service.sendSomethingWrong(chatId);
+                }
+            }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
